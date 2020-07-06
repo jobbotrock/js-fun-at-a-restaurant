f6192c8fd490c242f7a739e4e60db6679b794f7b
jobbot <joevarela83@gmail.com>
2020-07-05 19:05:10 -0600

    Complete iteration 1

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,61 @@
-function nameMenuItem() {
+////////////////////////////// nameMenuItem ////////////////////////////////////
 
-}
+function nameMenuItem(food) {
+  return 'Delicious ' + food;
+};
+
+
+////////////////////////////// CreateMenuItem //////////////////////////////////
+
+function createMenuItem(name, price, type) {
+  var menuItem = {
+    name: name,
+    price: price,
+    type: type
+  }
+  return menuItem;
+};
+
+////////////////////////////// addIngredients //////////////////////////////////
+
+function addIngredients(ingredient, ingredients) {
+  for (var i = 0; i < ingredients.length; i++) {
+    if (ingredient === ingredients[i]) {
+      return;
+    }
+  }
+  return ingredients.push(ingredient);
+};
+
+////////////////////////////// formatPrice /////////////////////////////////////
+
+function formatPrice(menuItem) {
+  return '$' + menuItem;
+};
+
+////////////////////////////// decreasePrice ///////////////////////////////////
+
+function decreasePrice(menuItem) {
+  return menuItem * .9;
+};
+
+////////////////////////////// createRecipe ///////////////////////////////////
+
+function createRecipe(title, ingredients, type) {
+  var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: type
+  }
+  return recipe;
+};
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
