7ec6ac144ed470556f65c6b117ee82907a09c90e
jobbot <joevarela83@gmail.com>
2020-07-05 19:40:54 -0600

    Complete iteration 2

--- src/order.js
+++ src/order.js
@@ -1,7 +1,52 @@
+////////////////////////////// nameMenuItem ////////////////////////////////////
+
+function takeOrder(order, deliveryOrders) {
+  if (deliveryOrders.length === 3) {
+    return;
+  }
+  deliveryOrders.push(order);
+};
+
+////////////////////////////// refundOrder /////////////////////////////////////
+
+function refundOrder(orderNumber, deliveryOrders) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (orderNumber === deliveryOrders[i].orderNumber) {
+      deliveryOrders.splice(i, 1);
+    }
+  }
+};
+
+////////////////////////////// listItems ///////////////////////////////////////
+
+function listItems(deliveryOrders) {
+ console.log(deliveryOrders)
+
+  for (var i = 0; i < deliveryOrders.length - 1; i++) {
+    if (deliveryOrders.length > 1) {
+      orderList = deliveryOrders[i].item;
+    } else if (deliveryOrders.length < 1) {
+      orderList = deliveryOrders[i].item + space;
+    }
+  }
+  return orderList;
+};
+
+////////////////////////////// searchOrder /////////////////////////////////////
+
+function searchOrder(deliveryOrders, order) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (order === deliveryOrders[i].item) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
