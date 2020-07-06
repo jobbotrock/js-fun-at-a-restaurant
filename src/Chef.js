7d7246ffbb1976153a33951b991481ad8bacc05c
jobbot <joevarela83@gmail.com>
2020-07-05 20:37:08 -0600

    Complete iteration 4

--- src/Chef.js
+++ src/Chef.js
@@ -1,7 +1,27 @@
 class Chef {
-  constructor() {
-    
+  constructor(name, restaurant) {
+    this.name = name;
+    this.restaurant = restaurant;
   }
-}
+
+  greetCustomer(customerName, isMorning) {
+    if (isMorning === true) {
+      return `Good morning, ${customerName}!`;
+    } else {
+      return `Hello, ${customerName}!`;
+    }
+  }
+
+  checkForFood(foodItem) {
+    if (foodItem.name === foodItem.name && foodItem.type === "breakfast") {
+      return `Yes, we're serving ${foodItem.name} today!`;
+    } else if (foodItem.name !== foodItem.type) {
+      return `Sorry, we aren't serving ${foodItem.name} today.`;
+    }
+  }
+};
+
+
+
 
 module.exports = Chef;
